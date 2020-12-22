d61b8581aef8012428429c790e8fac5924ecda2f
John Zeichman <jzeichman@gmail.com>
2020-12-22 15:08:52 -0500

    menu work

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const openMenu = document.querySelector(".openMenu");
 const closeMenu = document.querySelector('.closeMenu');
 openMenu.addEventListener("click", openMenuBar);
 function openMenuBar(){
- console.log('beens for sale')
- menu.style.left = "20%";
-}
+menu.style.right = "20%";
+};
 closeMenu.addEventListener("click", closeMenuBar);
 function closeMenuBar(){
- console.log('beens for sale')
- menu.style.left = "100%";
+ menu.style.right = "100%";
+ 
 }
 
