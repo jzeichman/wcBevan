ac339478d21ed5b4552e21ce6a84a8910acdb505
John Zeichman <jzeichman@gmail.com>
2021-01-03 21:08:12 -0500

    svg

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+const body = document.querySelector('body');
 const menu = document.querySelector('.menu');
 const openMenu = document.querySelector(".openMenu");
 const closeMenu = document.querySelector('.closeMenu');
 openMenu.addEventListener("click", openMenuBar);
 function openMenuBar(){
 menu.style.right = "20%";
+body.style.overflow = "hidden";
+
 };
 closeMenu.addEventListener("click", closeMenuBar);
 function closeMenuBar(){
  menu.style.right = "100%";
+ body.style.overflow = "scroll";
+ console.log("beens")
  
 }
 
