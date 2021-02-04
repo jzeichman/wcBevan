ca6b4b1f369f6b15c15b6f19a1d5471dc156baf0
John Zeichman <jzeichman@gmail.com>
2021-02-04 16:00:31 -0500

    work on mural gallery

---
+++ script.js
@@ -0,0 +1,5 @@
+const rtArrow = document.querySelector(".rightArrow");
+const imageTrack =document.querySelector(".imgTrack");
+rtArrow.addEventListener("click",function(){
+ imageTrack.style.transform = "translateX(-100%)";
+ console.log("beens");})
